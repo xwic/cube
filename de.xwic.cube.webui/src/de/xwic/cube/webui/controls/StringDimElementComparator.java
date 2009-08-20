83fbc75d29c681ae32e29fcb719efd1ed6f84ecf
Ronny Pfretzschner <Ronny.Pfretzschner@netapp.com>
2009-08-20 09:24:28 +0000

    added comparator

---
+++ de.xwic.cube.webui/src/de/xwic/cube/webui/controls/StringDimElementComparator.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.xwic.cube.webui.controls;
+
+import java.util.Comparator;
+
+import de.xwic.cube.IDimensionElement;
+
+/**
+ * @author ronny
+ *
+ */
+public class StringDimElementComparator implements Comparator<IDimensionElement> {
+
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	public int compare(IDimensionElement o1, IDimensionElement o2) {
+		if (o1 == null && o2 == null) {
+			return 0;
+		}
+		if (o1 == null) {
+			return -1;
+		}
+		if (o2 == null) {
+			return 1;
+		}
+		
+		return o1.getKey().compareTo(o2.getKey());
+	}
+}
