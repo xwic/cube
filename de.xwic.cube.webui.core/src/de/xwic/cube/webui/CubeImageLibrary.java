91441b7800f4051cf139f700494012fb2ef8728d
bogdan <Bogdan.Bugarschi@netapp.com>
2014-03-17 18:13:51 +0200

    Added CubeImageLibrary for image refs related to cube4s

---
+++ de.xwic.cube.webui.core/src/de/xwic/cube/webui/CubeImageLibrary.java
@@ -0,0 +1,20 @@
+package de.xwic.cube.webui;
+
+import de.jwic.base.ImageRef;
+
+/**
+ * Cube Image Library.<br>
+ * Contains ImageRefs used or useful for the cubes 
+ * @author bogdan
+ *
+ */
+public final class CubeImageLibrary {
+	private CubeImageLibrary() {
+	}
+	
+    public static final ImageRef IMAGE_EXPAND = new ImageRef("icons/expandall.gif");
+    public static final ImageRef IMAGE_COLLAPSE = new ImageRef("icons/collapseall.gif");
+    public static final ImageRef IMAGE_FILTTER = new ImageRef("icons/filter.png");
+    
+
+}
