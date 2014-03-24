c7196506fd566d5b5ed9654f6cde7aaf3ed643aa
bogdan <Bogdan.Bugarschi@netapp.com>
2014-03-24 11:48:24 +0200

    Added rollover to cube viewer

--- de.xwic.cube.webui.core/src/de/xwic/cube/webui/viewer/CubeViewer.static.js
+++ de.xwic.cube.webui.core/src/de/xwic/cube/webui/viewer/CubeViewer.static.js
@@ -1,13 +1,22 @@
 Cube.CubeViewer = (function($,escape){
 	return {
 		initialize : function (options){
+			var cStyle;
+			var cId;
+			var tblId ='#'+escape(options.controlID); 
 			if(options.fixedHeaders){
-				$('#'+escape(options.controlID)).fixedHeaderTable({ 
+				$(tblId).fixedHeaderTable({ 
 					footer: false, 
 					cloneHeadToFoot: false, 
 					fixedColumns: 1 
 				});
+				
 			}
+			$(tblId).find("tr").mouseover(function() {
+				$(this).addClass("hover");
+			}).mouseout(function() {
+				$(this).removeClass("hover");
+			});
 		},
 		destroy: function(options){
 			
