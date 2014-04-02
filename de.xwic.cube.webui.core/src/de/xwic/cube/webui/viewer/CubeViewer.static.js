644f80ada9b82f3663193fececacb55dbbacf6f1
bogdan <Bogdan.Bugarschi@netapp.com>
2014-04-02 18:43:57 +0300

    Fixed height resize

--- de.xwic.cube.webui.core/src/de/xwic/cube/webui/viewer/CubeViewer.static.js
+++ de.xwic.cube.webui.core/src/de/xwic/cube/webui/viewer/CubeViewer.static.js
@@ -1,17 +1,20 @@
 Cube.CubeViewer = (function($,escape){
 	var COLUMN_WIDTH = 80,
 		FIRST_COLUMN_WIDTH = 266,
-		SCROLL_BAR_SIZE = 20;
+		SCROLL_BAR_SIZE = 15,
+		HEIGHT_OF_THE_REST_OF_THE_WINDOW = 380; //aproximate height of everything else
+	
 	
 	function sizeSetter(table){
 		table.fixedHeaderTable('destroy');
 		var x = ((table.find('tr').first().find('th').length -1 ) * COLUMN_WIDTH + FIRST_COLUMN_WIDTH+ SCROLL_BAR_SIZE);
 		x = x > table.parent().width() ? table.parent().width() : x;
+		var y = parseInt(($(window).height()-HEIGHT_OF_THE_REST_OF_THE_WINDOW),10);
 		table.fixedHeaderTable({ 
 			footer: false, 
 			cloneHeadToFoot: false, 
 			fixedColumns: 1,
-			height: "600px",
+			height: y+"px",
 			width: x+"px"
 		});
 	}
